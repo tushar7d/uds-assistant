7ac5267bf8bf5b6b736db575394f61b5b9e0281e
Tushar Debnath <txdebnath@gmail.com>
2020-05-01 20:25:36 +0530

    changes

--- src/code.ts
+++ src/code.ts
@@ -1,65 +1,24 @@
-let count = 0
-function traverse(node) {
-  if ("children" in node) {
-    count++
-    if (node.type !== "INSTANCE") {
-      for (const child of node.children) {
-        traverse(child)
+let allnodes = figma.currentPage;
+
+let n = 0;
+
+let FindErrors = (nodes) => {
+  n++;
+
+  console.log(nodes.name + " " + n);
+
+  if ("children" in nodes) {
+
+  
+    for (const child of nodes.children) {
+      if (child.type === "TEXT"){
+
+        console.log("node is Text")
       }
+      FindErrors(child);
     }
   }
-}
-traverse(figma.root) 
-
-
-const textNodesWithNoTextStyle = figma.currentPage.findAll(
-  (node) => node.type === "TEXT" && node.textStyleId === ""
-);
-
-
-
-console.log(figma.getNodeById(textNodesWithNoTextStyle[0].id).name);
-
-const textNodesWithNoFillStyle = figma.currentPage.findAll(
-  (node) => node.type === "TEXT" && node.fillStyleId === ""
-);
-
-const detachedNodes = figma.currentPage.findAll(
-  (node) =>
-    node.type != "INSTANCE" &&
-    node.type === "FRAME" &&
-    node.name.search("/") > -1 &&
-    node.name.search("_") === -1
-);
-const partialNodes = figma.currentPage.findAll(
-  (node) =>
-    node.type != "INSTANCE" &&
-    node.type === "FRAME" &&
-    node.name.search("/") > -1 &&
-    node.name.search("_") === 0
-);
-
-
-let getNameFromId = (id) => {
-  let name = figma.getNodeById(id).name;
-
-  return name;
 };
+FindErrors(allnodes);
 
-
-figma.showUI(__html__);
-
-figma.ui.postMessage({
-  count: {
-    all:
-      textNodesWithNoFillStyle.length +
-      textNodesWithNoTextStyle.length +
-      detachedNodes.length +
-      partialNodes.length,
-    type: textNodesWithNoTextStyle.length,
-    color: textNodesWithNoFillStyle.length,
-    comp: detachedNodes.length + partialNodes.length,
-  },
-});
-
-figma.ui.resize(500, 350);
+figma.closePlugin();
