3664e67950652c5e4df508f4fcb4a9d17394cbdb
Tushar Debnath <txdebnath@gmail.com>
2020-05-03 00:30:44 +0530

    changes

--- src/common.js
+++ src/common.js
@@ -1,9 +1,7 @@
-export const selectNode = (id)=>{
-
-    parent.postMessage({ pluginMessage: { type: 'select-node', id } }, '*')
-
-}
+export const selectNode = (id) => {
+  parent.postMessage({ pluginMessage: { type: "select-node", id } }, "*");
+};
 
 export function handleClick(id) {
-    selectNode(id);
-  }
+  selectNode(id);
+}
